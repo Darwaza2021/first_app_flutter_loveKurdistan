977d410fe18747f761c91c88028f1cc33464d02a
Daro <darwaza.farhad8@gmail.com>
2021-12-18 19:51:30 +0300

    my first commit

---
+++ android/app/src/main/kotlin/com/example/darwaza/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.darwaza
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:darwaza/screen/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const darwaza());
+}
+
+      
